f4b947360675dc3863a892e31a6eda1a625c0004
Duarte Andrés <duarteandresj@gmail.com>
2022-10-23 00:40:46 -0300

    Se agrega la validacion del campo contacto

--- js/script.js
+++ js/script.js
@@ -1,29 +1,52 @@
 // Funcion del boton login
-const $btnLogin=document.querySelector(".btn-login");
-$btnLogin.addEventListener("click",()=>{
-    location.href="login.html"}
-)
+const $btnLogin = document.querySelector(".btn-login");
+$btnLogin.addEventListener("click", () => {
+  location.href = "login.html";
+});
 // Validacion de formulario de contacto
-const $formNombre=document.getElementById("form-nombre");
-const $formText=document.getElementById("form__textarea");
-const $btnEnvio=document.querySelector(".form__btn");
+const $formNombre = document.getElementById("form-nombre");
+const $formText = document.getElementById("form__textarea");
+const $btnEnvio = document.querySelector(".form__btn");
+const newSpan1 = document.createElement("span1");
+newSpan1.classList.add("alerta");
+const newSpan2 = document.createElement("span2");
+newSpan2.classList.add("alerta");
 
-$btnEnvio.addEventListener("click",(event)=>{
-    event.preventDefault();
-    let nombreCorrecto=$formNombre.value.length<40;
-    console.log(nombreCorrecto);
-    
-})
+function validaMaximoCaracteres(formString, largo) {
+  return (formString.length > largo);
+}
+function validaTextoVacio(cualquierString) {
+  return cualquierString.trim().length > 0;
+}
 
+function validaNombre() {
+  nombre = $formNombre.value;
+  if (!validaTextoVacio(nombre)) {
+    newSpan1.textContent = "El campo no puede estar vacio";
+    $formNombre.insertAdjacentElement("afterend", newSpan1);
+  } else if (validaMaximoCaracteres(nombre, 40)) {
+    newSpan1.textContent = "Maximo de caracteres excedido";
+    $formNombre.insertAdjacentElement("afterend", newSpan1);
+  } else {
+    newSpan1.remove();
+  }
+}
 
-/*El formulario de contacto debe contener algunas validaciones como:
+function validaMensaje() {
+  mensaje = $formText.value;
+  if (!validaTextoVacio(mensaje)) {
+    newSpan2.textContent = "El campo no puede estar vacio";
+    $formText.insertAdjacentElement("afterend", newSpan2);
+  } else if (validaMaximoCaracteres(mensaje, 120)) {
+    newSpan2.textContent = "Maximo de caracteres excedido";
+    $formText.insertAdjacentElement("afterend", newSpan2);
+  } else {
+    newSpan2.remove();
+  }
+}
 
-- Campo de nombre: máximo 40 caracteres.
-- Campo de mensaje: máximo 120 caracteres.
-- Validación para que los campos no queden vacíos o en blanco.
-- Debe mostrar al usuario los mensajes de error en caso de que no se cumplan las validaciones.
-- Botón Enviar mensaje: envía el mensaje si el formulario está correctamente cumplimentado.
-
-Si necesita ayuda le sugerimos:
-https://app.aluracursos.com/course/javascript-web-validacion-formularios-html5
-*/
+$btnEnvio.addEventListener("click", (event) => {
+  event.preventDefault();
+  validaNombre();
+  validaMensaje();
+});
